dd8a5e56b2802a0840b629ee62b39fce600d5970
Varis0282 <varisrajakforth11@gmail.com>
2024-03-09 11:38:25 +0530

    Animations place in home page changed

--- src/common/components/live_classes/Live.js
+++ src/common/components/live_classes/Live.js
@@ -5,14 +5,14 @@ import { Live_Classes } from '../../animation';
 const Live = () => {
     return (
         <div className="flex w-full md:flex-row flex-col justify-center items-center px-4 mt-8">
-            <div className="md:w-2/5 w-full md:flex order-1 items-center justify-center" id='live-classes'>
+            <div className="md:w-2/5 w-full md:flex order-1 md:order-2 items-center justify-center" id='live-classes'>
                 <Lottie
                     animationData={Live_Classes}
                     loop={true}
                     autoplay={true}
                 />
             </div>
-            <div className="md:w-2/4 w-full order-2 flex flex-col md:pl-12 md:pr-6 px-2 py-2 gap-3">
+            <div className="md:w-2/4 w-full order-2 md:order-1 flex flex-col md:pl-12 md:pr-6 px-2 py-2 gap-3">
                 <p className='text-lg font-medium'>No don't need to watch those <span className="text-orange-500">boring tutorials</span> because you have</p>
                 <p className='md:text-5xl text-3xl leading-[1.3] font-medium'><span className="text-orange-500">Live CLasses</span> with <span className="text-orange-500">Recordings,</span></p>
                 <p className='text-lg font-medium'>so that you can have</p>
--- src/common/components/personalized_learning/Personalized_Learning.js
+++ src/common/components/personalized_learning/Personalized_Learning.js
@@ -5,14 +5,14 @@ import { Personalized_ex } from '../../animation';
 const Personalized_Learning = () => {
     return (
         <div className="flex w-full md:flex-row flex-col justify-center items-center px-4">
-            <div className="md:w-2/5 w-full md:flex order-1 items-center justify-center">
+            <div className="md:w-2/5 w-full md:flex order-1 md:order-2 items-center justify-center">
                 <Lottie
                     animationData={Personalized_ex}
                     loop={true}
                     autoplay={true}
                 />
             </div>
-            <div className="md:w-2/4 w-full flex flex-col order-2 md:pl-12 md:pr-6 px-2 py-2 gap-3">
+            <div className="md:w-2/4 w-full flex flex-col order-2 md:order-1 md:pl-12 md:pr-6 px-2 py-2 gap-3">
                 <p className='text-lg font-medium'>You can have <span className="text-orange-500">direct</span></p>
                 <p className='md:text-5xl text-3xl leading-[1.3] font-medium'><span className="text-orange-500">Interaction</span> with <span className="text-orange-500">Your Mentor Anytime</span></p>
                 <p className='text-lg font-medium'>just to discuss about</p>
